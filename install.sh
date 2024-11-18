b02484c8cb5890351633e3924b1542f87d0a5498
Johannes vom Dorp <johannes.vom.dorp@fkie.fraunhofer.de>
2024-11-18 12:40:43 +0100

    dropped webui install

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 ## Install Required Packages
 install_clean \
     build-essential \
@@ -15,42 +14,13 @@ cd radare2
 sys/install.sh
 cd ..
 
-
-## Build WebUI
-git clone https://github.com/dorpvom/radare2-webui.git
-cd radare2-webui
-make root
-
-make material
-make panel
-make enyo
-make tiles
-
-
-## Patch WebUI
-mv dist/enyo/r2core.js        ../radare2/shlr/www/enyo/r2core.js
-mv dist/enyo/r2app.js         ../radare2/shlr/www/enyo/r2app.js
-mv dist/r2.js                 ../radare2/shlr/www/f/r2.js
-mv dev/m/disasmNavProvider.js ../radare2/shlr/www/m/disasmNavProvider.js
-mv dev/m/disasmProvider.js    ../radare2/shlr/www/m/disasmProvider.js
-mv dev/m/hexchunkProvider.js  ../radare2/shlr/www/m/hexchunkProvider.js
-mv dev/m/r2.js                ../radare2/shlr/www/m/r2.js
-mv dev/p/r2core.js            ../radare2/shlr/www/p/r2core.js
-mv dist/t/app.js              ../radare2/shlr/www/t/app.js
-
-cd ..
-rm -rf /opt/app/radare2-webui
-
-
 ## Install Server
-git clone https://github.com/fkie-cad/fact_radare_viewer.git
+git clone -b change-r2-call https://github.com/dorpvom/fact_radare_viewer.git
 pip3 install -r fact_radare_viewer/requirements.txt
-
 
 ## Install Python Plugin
 pip3 install setuptools wheel
 pip3 install r2pipe
-
 
 ## Remove Redundant Packages
 apt-get remove -y \
@@ -59,6 +29,5 @@ apt-get remove -y \
     npm \
     python3-pip
 apt-get autoremove -y
-
 
 exit 0
